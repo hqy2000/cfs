8e534a5b20cb65b0444f0e1de2f7b10f31b36e91
Qingyang Hu <huqingyang2000@gmail.com>
2023-11-03 01:24:22 +0000

    Add protobuf and fuse3

--- main.cpp
+++ main.cpp
@@ -1,5 +1,18 @@
-#include <iostream>
+#define FUSE_USE_VERSION 31
 
-int main() {
-    std::cout<<"dcfs2";
+#include <iostream>
+#include "message.pb.h"
+#include "fuse3/fuse.h"
+
+struct fuse_operations dcfs2_fuse_oper {
+
+};
+
+int main(int argc, char *argv[]) {
+    std::cout<<"dcfs2"<<std::endl;
+
+    Message message;
+    message.set_id(2);
+
+    return fuse_main(argc, argv, &dcfs2_fuse_oper, nullptr);
 }
