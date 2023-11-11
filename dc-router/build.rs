27d24b19cfda9d39e9136fbff0288b210981a0cf
Qingyang Hu <me@hqy.moe>
2023-11-11 13:33:10 -0800

    Change folder structure

--- dc-router/build.rs
+++ dc-router/build.rs
@@ -1,6 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .type_attribute("data_capsule.DataCapsuleBlock", "#[derive(Hash)]")
-        .compile(&["proto/data_capsule.proto", "proto/router.proto"], &["proto"])?;
+        .compile(&["../proto/data_capsule.proto", "../proto/router.proto"], &["../proto"])?;
     Ok(())
 }
