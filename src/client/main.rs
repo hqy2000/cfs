1297131e7542e2c3e82c07daf29a64ab64fd6871
Qingyang Hu <huqingyang2000@gmail.com>
2023-11-12 08:41:16 +0000

    move files

---
+++ src/client/main.rs
@@ -0,0 +1,39 @@
+mod client;
+
+use clap::{Arg, ArgAction, Command};
+use fuser::MountOption;
+use crate::client::DCFS2;
+
+fn main() {
+    let matches = Command::new("hello")
+        .author("Christopher Berner")
+        .arg(
+            Arg::new("MOUNT_POINT")
+                .required(true)
+                .index(1)
+                .help("Act as a client, and mount FUSE at given path"),
+        )
+        .arg(
+            Arg::new("auto_unmount")
+                .long("auto_unmount")
+                .action(ArgAction::SetTrue)
+                .help("Automatically unmount on process exit"),
+        )
+        .arg(
+            Arg::new("allow-root")
+                .long("allow-root")
+                .action(ArgAction::SetTrue)
+                .help("Allow root user to access filesystem"),
+        )
+        .get_matches();
+    // env_logger::init();
+    let mountpoint = matches.get_one::<String>("MOUNT_POINT").unwrap();
+    let mut options = vec![MountOption::RO, MountOption::FSName("hello".to_string())];
+    if matches.get_flag("auto_unmount") {
+        options.push(MountOption::AutoUnmount);
+    }
+    if matches.get_flag("allow-root") {
+        options.push(MountOption::AllowRoot);
+    }
+    fuser::mount2(DCFS2, mountpoint, &options).unwrap();
+}
