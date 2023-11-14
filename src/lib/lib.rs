ffd3c0cf49b5ffcc900d2235cbcc07584c4b7edd
Qingyang Hu <huqingyang2000@gmail.com>
2023-11-14 22:32:35 +0000

    Make compiler happy

---
+++ src/lib/lib.rs
@@ -0,0 +1,5 @@
+mod client;
+mod fs;
+mod middleware;
+mod server;
+mod proto;
---
+++ src/lib/proto.rs
@@ -0,0 +1,9 @@
+mod block {
+    tonic::include_proto!("block");
+}
+pub mod data_capsule {
+    tonic::include_proto!("data_capsule");
+}
+pub mod middleware {
+    tonic::include_proto!("middleware");
+}
