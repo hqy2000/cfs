52b9df4a064e2c719555a8565c675d3f00ae2d36
Qingyang Hu <huqingyang2000@gmail.com>
2023-11-14 23:26:00 +0000

    inode server

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,40 +1,86 @@
 use std::collections::HashMap;
 use std::sync::Arc;
+use futures::future::join_all;
 use tokio::sync::Mutex;
 use tonic::transport::Server;
+use tonic::transport::server::Router;
 use lib::proto::block::data_capsule_block::Block;
-use lib::proto::block::{DataBlock, DataCapsuleBlock};
+use lib::proto::block::{DataBlock, DataCapsuleBlock, INodeBlock};
+use lib::proto::block::i_node_block::Kind;
 use lib::proto::data_capsule::data_capsule_server::DataCapsuleServer;
 use lib::proto::data_capsule::DataCapsuleServerData;
 use lib::server::MyDataCapsule;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
-    let router = MyDataCapsule {
+    let data_capsule_addr = "[::1]:50051".parse()?;
+    let data_capsule = MyDataCapsule {
         data: Arc::new(Mutex::new(
             DataCapsuleServerData {
                 id: Vec::new(),
                 write_pub_key: Vec::new(),
                 content: HashMap::from(
-                    [("testhash".to_string(), DataCapsuleBlock{
-                        prev_hash: "".to_string(),
-                        block: Some(Block::Data(DataBlock{
-                            data: Vec::from("dc server resp")
-                        })),
-                        signature: vec![],
-                    })]
+                    [
+                        ("file_hash".to_string(),
+                         DataCapsuleBlock {
+                             prev_hash: "".to_string(),
+                             block: Some(Block::Data(DataBlock {
+                                 data: Vec::from("dc server resp lololololol")
+                             })),
+                             signature: vec![],
+                         })]
                 ),
-                leafs: Vec::new()
+                leafs: Vec::new(),
             }
         )),
     };
 
-    Server::builder()
-        .add_service(DataCapsuleServer::new(router))
-        .serve(addr)
-        .await?;
+    let inodeCapsuleAddr = "[::1]:50052".parse()?;
+    let inodeCapsule = MyDataCapsule {
+        data: Arc::new(Mutex::new(
+            DataCapsuleServerData {
+                id: Vec::new(),
+                write_pub_key: Vec::new(),
+                content: HashMap::from(
+                    [
+                        ("root".into(), DataCapsuleBlock {
+                             prev_hash: "".to_string(),
+                             block: Some(Block::Inode(INodeBlock {
+                                 filename: vec![],
+                                 size: 0,
+                                 kind: Kind::Directory.into(),
+                                 hashes: vec![],
+                             })),
+                             signature: vec![],
+                         }),
+                        ("file".into(), DataCapsuleBlock {
+                            prev_hash: "root".to_string(),
+                            block: Some(Block::Inode(INodeBlock {
+                                filename: Vec::from("dc_file.txt"),
+                                size: 26,
+                                kind: Kind::RegularFile.into(),
+                                hashes: vec!["file_hash".into()],
+                            })),
+                            signature: vec![],
+                        })
+                    ]
+                ),
+                leafs: vec!["file".into()],
+            }
+        )),
+    };
 
+    let mut v = Vec::new();
+
+    v.push(Server::builder()
+        .add_service(DataCapsuleServer::new(data_capsule))
+        .serve(data_capsule_addr));
+
+    v.push(Server::builder()
+        .add_service(DataCapsuleServer::new(inodeCapsule))
+        .serve(inodeCapsuleAddr));
+
+    join_all(v).await;
     Ok(())
 }
 
@@ -44,16 +90,16 @@ mod tests {
     use crate::server::server::GetRequest;
 
     #[tokio::test]
-    async fn test_get() -> Result<(), Box<dyn std::error::Error>>  {
-        let mut client = DataCapsuleClient::connect("http://[::1]:50051").await?;
-
-        let request = tonic::Request::new(GetRequest {
-            block_hash: "testhash".to_string()
-        });
-
-        let response = client.get(request).await?;
-
-        println!("RESPONSE={:?}", response);
+    async fn test_get() -> Result<(), Box<dyn std::error::Error>> {
+        // let mut client = DataCapsuleClient::connect("http://[::1]:50051").await?;
+        //
+        // let request = tonic::Request::new(GetRequest {
+        //     block_hash: "testhash".to_string()
+        // });
+        //
+        // let response = client.get(request).await?;
+        //
+        // println!("RESPONSE={:?}", response);
         Ok(())
     }
 }
